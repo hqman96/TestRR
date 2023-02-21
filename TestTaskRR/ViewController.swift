2f1836d8f382b81af86167f461e017219b7400b7
Georgiy Kiselev <g.kiselev.iosdev@gmail.com>
2023-02-21 15:12:41 +0300

    Initial Commit

---
+++ TestTaskRR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskRR
+//
+//  Created by Георгий Киселев on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
